1962e46b10344db9c3b5ecec336f2ec070d2c391
Gustav <gustav@gustavnybro.com>
2023-09-04 08:36:24 +0200

    update gitignore

---
+++ 1-lektion/about_me.py
@@ -0,0 +1,2 @@
+def print_name(name: str, age: int, city: str, favorite_num: int):
+    print(f"Mit navn er {name}, og jeg er {age} år. Jeg er født i {city}, og mit yndlingstal er {favorite_num}.")
--- 1-lektion/main.py
+++ 1-lektion/main.py
@@ -1,1 +1,4 @@
-# Prog
+from about_me import print_name
+
+if __name__ == "__main__":
+    print_name("Mads", 24, "Aarhus", 7)
